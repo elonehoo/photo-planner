9b31ebf02ed56168ac4c1b5ab2f88f8ab25c2353
elonehoo <hi@elonehoo.me>
2023-08-14 14:17:47 +0800

    fix: remove tailwind
    close #3 #4

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
 import App from './App.vue'
-import '@unocss/reset/tailwind.css'
 import 'mobile-drag-drop/default.css'
 import 'uno.css'
 
